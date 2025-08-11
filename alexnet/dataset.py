0fc2db6442494e02ea5bd36afb8c61302b1d50a4
HieuHieu <145194277+hieubadao1775@users.noreply.github.com>
2025-08-12 06:13:33 +0700

    Add files via upload

---
+++ alexnet/dataset.py
@@ -0,0 +1,39 @@
+from torch.utils.data import Dataset
+import os
+from PIL import Image
+from torchvision.transforms import Compose
+
+
+class Animal(Dataset):
+    def __init__(self, root, transform=Compose([])):
+        super().__init__()
+        self.image_paths = []
+        self.labels = []
+        self.transform = transform
+        self.categories = sorted(os.listdir(root))
+
+        category_paths = [os.path.join(root, category) for category in self.categories]
+        for i, category_path in enumerate(category_paths):
+            image_paths = [os.path.join(category_path, image_file) for image_file in os.listdir(category_path)]
+            self.image_paths.extend(image_paths)
+            self.labels.extend([i] * len(image_paths))
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, item):
+        image_path = self.image_paths[item]
+        image = Image.open(image_path).convert("RGB")
+        image = self.transform(image)
+
+        label = self.labels[item]
+
+        return image, label
+
+if __name__ == '__main__':
+    data = Animal(root="../data/animals/train")
+
+    image, label = data.__getitem__(10020)
+
+    print(data.categories[label])
+    image.show()
